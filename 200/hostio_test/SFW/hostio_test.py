48b87ba9e8ea062228f4ea34b2c1557d3eccbee0
xesscorp <xesscorp@xess.com>
2011-08-23 12:03:09 -0400

    Replaced all the ctypes lists with regular lists since xstoolsapi.py can handle those now. Also output the counter value in decimal as well as binary.

--- 200/hostio_test/SFW/hostio_test.py
+++ 200/hostio_test/SFW/hostio_test.py
@@ -37,78 +37,53 @@ from xstoolsapi import *
 import sys
 import random
 
-USB_ID = 0   # This is the USB index for the XuLA board connected to the host PC.
-
+USB_ID = 0  # This is the USB index for the XuLA board connected to the host PC.
 
 print '''\n\n\n
 ##################################################################
 # Test the single register in the FPGA hostio_test circuit.
 ##################################################################
 '''
-REG_ID = 1 # This is the identifier for the single register in the FPGA.
-addrWidth = c_uint() # Stores the address width of the register.
-dataWidth = c_uint() # Stores the data width of the register.
+REG_ID = 1  # This is the identifier for the single register in the FPGA.
+reg = XsMem(USB_ID, REG_ID)  # Create an object for reading/writing the register.
 
-# Get a handle for the register in the FPGA.
-reg = MemInit(USB_ID, REG_ID, byref(addrWidth), byref(dataWidth))
-if reg == 0:
-    print "Couldn't get a handle for the register!"
-    sys.exit()
-
-# Create variables for reading/writing the register.
-addr = c_uint(0) # Set the address to zero.
-writeData = c_ulonglong() # Stores the data to write to the register.
-readData = c_ulonglong() # Stores the data read back from the register.
-
-# Repeatedly write & read the register.
-numTrials = 1000
+NUM_TRIALS = 1000  # Number of register reads and writes to do.
+REG_MAX_DATA = 2 ** reg.mDataWidth - 1  # Maximum value that can be stored in register.
 errorCntr = 0
-for i in range(0,numTrials):
-    writeData.value = random.randint(0,2**(dataWidth.value-1)) # Generate a random value.
-    MemWrite(reg,addr,byref(writeData),1) # Write it to the register.
-    MemRead(reg,addr,byref(readData),1) # Read back the register contents.
-    if readData.value != writeData.value: # Compare value read back to value written.
-        errorCntr += 1 # Record error if value read back doesn't match value written.
-print "Register was written and read back {0} times and {1} errors occurred.".format(numTrials,errorCntr)
+for i in range(0, NUM_TRIALS):
+    wrData = random.randint(0, REG_MAX_DATA + 1)
+    reg.Write(0, wrData)
+    rdData = reg.Read(0)
+    errorCntr += (1 if wrData != rdData else 0)
 
+# Report any comparison errors.
+print 'Register was written and read back {0} times and {1} errors occurred.'.format(NUM_TRIALS, errorCntr)
 
 print '''\n\n\n
 ##################################################################
 # Test the block RAM in the FPGA hostio_test circuit.
 ##################################################################
 '''
-BRAM_ID = 2 # This is the identifier for the BRAM in the FPGA.
-addrWidth = c_uint() # Stores the address width of the BRAM.
-dataWidth = c_uint() # Stores the data width of the BRAM.
+BRAM_ID = 2  # This is the identifier for the BRAM in the FPGA.
+bram = XsMem(USB_ID, BRAM_ID)  # Create an object for reading/writing the BRAM.
 
-# Get a handle for the BRAM in the FPGA.
-bram = MemInit(USB_ID, BRAM_ID, byref(addrWidth), byref(dataWidth))
-if bram == 0:
-    print "Couldn't get a handle for the block RAM!"
-    sys.exit()
+# Create constants and lists for reading/writing the BRAM.
+BRAM_SIZE = 2 ** bram.mAddrWidth  # Number of words in the BRAM.
+BRAM_MAX_DATA = 2 ** bram.mDataWidth - 1  # Maximum value that can be stored in BRAM word.
+# Create a list of random integers to write to the BRAM.
+wrBram = [random.randint(0, BRAM_MAX_DATA + 1) for i in range(0, BRAM_SIZE)]
+# Create a list of integers to receive the values read from the BRAM.
+rdBram = [0] * BRAM_SIZE
 
-# Create variables and arrays for reading/writing the BRAM.
-addr = c_uint() # Stores the BRAM address.
-bramSize = 2**addrWidth.value
-wrBram = (c_ulonglong * bramSize)()
-rdBram = (c_ulonglong * bramSize)()
-
-# Generate an array of random data values.
-for i in range(0, bramSize):
-    wrBram[i] = random.randint(0,2**(dataWidth.value-1)-1)
-
-MemWrite(bram,0,wrBram,bramSize) # Write array of random numbers to BRAM.
-MemRead(bram,0,rdBram,bramSize) # Read back contents of BRAM.
+# Here's the important part: actually writing values to the BRAM and reading them back.
+bram.Write(0, wrBram)  # Write the list of values to the BRAM.
+bram.Read(0, rdBram)  # Read the list of values from the BRAM.
 
 # Compare the values read back from the BRAM with the values written to it.
-errorCntr = 0
-for i in range(0,bramSize):
-    if rdBram[i] != wrBram[i]:
-        errorCntr += 1
+errorCntr = sum(map(lambda a, b: (1 if a != b else 0), rdBram, wrBram))
 
-# Report any comparison errors.        
-print "{0} BRAM locations were written and read back and {1} errors were found.".format(bramSize,errorCntr)
-
+# Report any comparison errors.
+print '{0} BRAM locations were written and read back and {1} errors were found.'.format(BRAM_SIZE, errorCntr)
 
 print '''\n\n\n
 ##################################################################
@@ -116,37 +91,29 @@ print '''\n\n\n
 ##################################################################
 '''
 CNTR_ID = 3  # This is the identifier for the counter in the FPGA.
-numInputs = c_uint()  # Stores the number of inputs to the counter.
-numOutputs = c_uint()  # Stores the number of outputs from the counter.
-
-# Get a handle for the counter in the FPGA.
-cntr = DutInit(USB_ID, CNTR_ID, byref(numInputs), byref(numOutputs))
-if cntr == 0:
-    print "Couldn't get a handle for the counter!"
-    sys.exit()
-
-# Create arrays for the inputs/outputs to/from the counter.
-inputs = (c_ubyte * numInputs.value)()
-outputs = (c_ubyte * numOutputs.value)()
-
+cntr = XsDut(USB_ID, CNTR_ID)
+cntBits = [0] * cntr.mNumOutputs
+MAX_CNT = 2 ** cntr.mNumOutputs - 1
 # These are the settings for the control input of the counter.
 INCREMENT = 1
 DECREMENT = 0
 
 print 'First, we increment:'
-inputs[0] = INCREMENT
-for i in range(0, 16):
-    DutWrite(cntr, inputs, numInputs) # Write the control signal and pulse the counter clock input.
-    DutRead(cntr, outputs, numOutputs) # Read the new value of the counter.
-    PrintBits(outputs) # Print the counter bits.
+for i in range(0, MAX_CNT + 1):
+    cntr.Write(INCREMENT)  # Write the control signal and pulse the counter clock input.
+    print '{0:4d} :'.format(cntr.Read()),   # Print the counter's decimal value.
+    cntr.Read(cntBits)  # Read the value of the counter again, but now as a list of individual bits.
+    PrintBits(cntBits)  # Print the counter bits.
 
 print '\nThen, we decrement:'
-inputs[0] = DECREMENT
-for i in range(0, 16):
-    DutWrite(cntr, inputs, numInputs)
-    DutRead(cntr, outputs, numOutputs)
-    PrintBits(outputs)
+for i in range(0, MAX_CNT + 1):
+    cntr.Write(DECREMENT)  # Write the control signal and pulse the counter clock input.
+    print '{0:4d} :'.format(cntr.Read()),   # Print the counter's decimal value.
+    cntr.Read(cntBits)  # Read the value of the counter again, but now as a list of individual bits.
+    PrintBits(cntBits)  # Print the counter bits.
 
-print '\n\n\nWaiting in infinite loop so you can examine the results...'
-while(True):
+print '''\n\n
+Waiting in an infinite loop so you can examine the results...
+'''
+while True:
     pass
