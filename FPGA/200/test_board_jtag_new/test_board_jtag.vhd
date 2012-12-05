24865b4c68ef6ec60e59bb7e1b6880d8becb61c0
Dave Vandenbout <xesscorp@xess.com>
2012-12-04 22:16:58 -0500

    Synced test initiation signal to test core clock.

--- FPGA/200/test_board_jtag_new/test_board_jtag.vhd
+++ FPGA/200/test_board_jtag_new/test_board_jtag.vhd
@@ -26,43 +26,40 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use IEEE.numeric_std.all;
 use work.CommonPckg.all;
-use work.HostIoPckg.HostIoToDut;
+use work.HostIoPckg.all;
 use work.TestBoardCorePckg.all;
 use work.ClkgenPckg.all;
-
-library UNISIM;
-use UNISIM.VComponents.all;
+use work.SyncToClockPckg.all;
 
 
 entity test_board_jtag is
   generic(
-    BASE_FREQ_G   : real    := 12.0;    -- base frequency in MHz
-    CLK_MUL_G     : natural := 25;      -- multiplier for base frequency
-    CLK_DIV_G     : natural := 3;       -- divider for base frequency
+    BASE_FREQ_G   : real    := 12.0;    -- Base frequency in MHz.
+    CLK_MUL_G     : natural := 25;      -- Multiplier for base frequency.
+    CLK_DIV_G     : natural := 3;       -- Divider for base frequency.
     PIPE_EN_G     : boolean := true;
-    DATA_WIDTH_G  : natural := 16;      -- width of data
-    HADDR_WIDTH_G : natural := 23;      -- host-side address width
-    SADDR_WIDTH_G : natural := 12;      -- SDRAM address bus width
-    NROWS_G       : natural := 4096;    -- number of rows in each SDRAM bank
-    NCOLS_G       : natural := 512;     -- number of words in each row
-    -- beginning and ending addresses for the entire SDRAM
+    DATA_WIDTH_G  : natural := 16;      -- Width of data.
+    HADDR_WIDTH_G : natural := 23;      -- Host-side address width.
+    SADDR_WIDTH_G : natural := 12;      -- SDRAM address bus width.
+    NROWS_G       : natural := 4096;    -- Number of rows in each SDRAM bank.
+    NCOLS_G       : natural := 512;     -- Number of words in each row.
+    -- Beginning and ending addresses for the entire SDRAM.
     BEG_ADDR_G    : natural := 16#00_0000#;
     END_ADDR_G    : natural := 16#7F_FFFF#;
-    -- beginning and ending address for the memory tester
+    -- Beginning and ending address for the memory tester.
     BEG_TEST_G    : natural := 16#00_0000#;
---    END_TEST_G    : natural := 16#40_0000# -- Board will fail with this.
-    END_TEST_G    : natural := 16#3F_FFFF#  -- Board will pass with this.
+    END_TEST_G    : natural := 16#3F_FFFF#
     );
   port(
-    fpgaClk_i : in    std_logic;  -- main clock input from external clock source
-    sdClk_o   : out   std_logic;        -- clock to SDRAM
-    sdClkFb_i : in    std_logic;        -- SDRAM clock comes back in
-    sdRas_bo  : out   std_logic;        -- SDRAM RAS
-    sdCas_bo  : out   std_logic;        -- SDRAM CAS
-    sdWe_bo   : out   std_logic;        -- SDRAM write-enable
-    sdBs_o    : out   std_logic;        -- SDRAM bank-address
-    sdAddr_o  : out   std_logic_vector(SADDR_WIDTH_G-1 downto 0);  -- SDRAM address bus
-    sdData_io : inout std_logic_vector(DATA_WIDTH_G-1 downto 0)  -- data bus to/from SDRAM
+    fpgaClk_i : in    std_logic;  -- Main clock input from external clock source.
+    sdClk_o   : out   std_logic;        -- Clock to SDRAM.
+    sdClkFb_i : in    std_logic;        -- SDRAM clock comes back in.
+    sdRas_bo  : out   std_logic;        -- SDRAM RAS.
+    sdCas_bo  : out   std_logic;        -- SDRAM CAS.
+    sdWe_bo   : out   std_logic;        -- SDRAM write-enable.
+    sdBs_o    : out   std_logic;        -- SDRAM bank-address.
+    sdAddr_o  : out   std_logic_vector(SADDR_WIDTH_G-1 downto 0);  -- SDRAM address bus.
+    sdData_io : inout std_logic_vector(DATA_WIDTH_G-1 downto 0)  -- Data bus to/from SDRAM.
     );
 end entity;
 
@@ -71,33 +68,40 @@ architecture arch of test_board_jtag is
 
   constant FREQ_G : real := (BASE_FREQ_G * real(CLK_MUL_G)) / real(CLK_DIV_G);
   signal clk_s    : std_logic;
-
   signal reset_s         : std_logic;
+  signal syncedReset_s   : std_logic;
   signal test_ctrl_s     : std_logic_vector(0 downto 0);
-  signal test_progress_s : std_logic_vector(1 downto 0);  -- progress of the test
-  signal test_failed_s   : std_logic;  -- true if an error was found during the test
+  signal test_progress_s : std_logic_vector(1 downto 0);  -- Progress of the test.
+  signal test_failed_s   : std_logic;  -- True if an error was found during the test.
   signal signature_s     : std_logic_vector(31 downto 0) := x"A50001A5";
   signal test_status_s   : std_logic_vector(34 downto 0);
 begin
 
   u0 : Clkgen
     generic map (BASE_FREQ_G => BASE_FREQ_G, CLK_MUL_G => CLK_MUL_G, CLK_DIV_G => CLK_DIV_G)
-    port map(I               => fpgaClk_i, O => sdClk_o);
+    port map(I               => fpgaClk_i, clkToLogic_o => sdClk_o);
 
-  clk_s <= sdClkFb_i;  -- main clock is SDRAM clock fed back into FPGA
-
-  reset_s       <= test_ctrl_s(0);
-  test_status_s <= signature_s & test_failed_s & test_progress_s;
+  clk_s <= sdClkFb_i;  -- Main clock is SDRAM clock fed back into FPGA.
 
   u1 : HostIoToDut
-    generic map(SIMPLE_G => true)
+    generic map(SIMPLE_G => true, FPGA_DEVICE_G => SPARTAN3A)
     port map(
       vectorFromDut_i => test_status_s,
       vectorToDut_o   => test_ctrl_s
       );
+  reset_s       <= test_ctrl_s(0);
+  test_status_s <= signature_s & test_failed_s & test_progress_s;
 
-  -- board diagnostic unit
-  u2 : TestBoardCore
+  -- Sync reset signal from HostIoToDut to TestBoardCore.      
+  u2 : SyncToClock
+    port map (
+      clk_i      => clk_s,
+      unsynced_i => reset_s,
+      synced_o   => syncedReset_s
+      );
+
+  -- Board diagnostic unit.
+  u3 : TestBoardCore
     generic map(
       FREQ_G        => FREQ_G,
       PIPE_EN_G     => PIPE_EN_G,
@@ -125,4 +129,3 @@ begin
       );
 
 end architecture;
-
